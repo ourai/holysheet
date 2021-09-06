3582b3dc5d92f5429fd45579f488f4c574002274
Ourai Lin <ourairyu@gmail.com>
2021-09-06 10:04:53 +0800

    chore: expose `Table`

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { default as XSpreadsheet } from '@wotaware/x-spreadsheet';
+export { default as Table } from './table';
 export * from './sheet';
 export { Holysheet as default } from './spreadsheet';
